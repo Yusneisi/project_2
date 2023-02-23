b9a61dcecab0278d3a39a7bf5b35465f1b27d4b1
Yusneisi <yusneisidaz@gmail.com>
2023-02-23 10:16:53 -0400

    Agregue el archivo valInt.py

---
+++ validation/valInt.py
@@ -0,0 +1,29 @@
+def valInt (num, array = None):
+    variable = True
+    if not isinstance(num, int):
+        variable = False
+        return variable
+    
+    if array != None:
+        if isinstance(array, tuple):
+            if array[0] >= array[-1]:
+                raise ValueError("The range provided is not ordered" \
+                    "in an increasing way")
+            elif len(array) != 2:
+                raise ValueError("The range provided has more than" \
+                    "two elements")
+            elif num <= array[0] or num >= array[-1]:
+                variable = False
+        elif isinstance(array, list):
+            if array[0] >= array[-1]:
+                raise ValueError("The range provided is not ordered" \
+                    " in an increasing way")
+            elif len(array) != 2:
+                raise ValueError("The range provided has more than" \
+                    " two elements")
+            elif num < array[0] or num > array[-1]:
+                variable = False
+        else:
+            raise TypeError("The second argument is not the right type")
+
+    return variable 
